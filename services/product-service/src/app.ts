018509765efab8ae247daaa0419f29f3c0038c41
ansabazys <ansabazys@gmail.com>
2026-01-09 10:24:28 +0530

    refactor(route): change route name

--- services/product-service/src/app.ts
+++ services/product-service/src/app.ts
@@ -3,5 +3,5 @@ import categoryRoute from './api/routes/category.routes.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.use('/api/v1/categories', categoryRoute);
+app.use('/api/v1/category', categoryRoute);
 export default app;
